3083c576915f1d5a2874aa3693106b05dcd6ee4d
Nikita Belkin <76586422+ERKYNIS@users.noreply.github.com>
2024-05-08 22:59:49 +0300

    Add files via upload

--- modules/wiki.py
+++ modules/wiki.py
@@ -4,6 +4,8 @@ import datetime
 
 wiki = Blueprint('wiki', __name__, template_folder='templates')
 
+wiki_categories = {'characters': "Персонажи", "weapons": "Оружие", "enemies": "Враги", "gameplay": "Геймплей"}
+
 
 def databaserequest(req):
     con = sqlite3.connect("database.db", check_same_thread=False)
@@ -18,47 +20,59 @@ def databaserequest(req):
 
 @wiki.route('/wiki')
 def main_wiki():
-    wikis = databaserequest(f'''SELECT id, title, author, datetime FROM wiki''')
-    return render_template('wiki/wiki.html', wikis=wikis)
+    return render_template('wiki/wiki.html', session=session, title="База данных Ишимуры")
+
+
+@wiki.route('/wiki/<category>')
+def categories(category):
+    wikis = databaserequest(f'''SELECT id, title, author, datetime FROM wiki WHERE category='{category}' ''')
+    return render_template('wiki/wiki_categories.html', session=session,
+                           title=f"{wiki_categories[category]} - База данных Ишимуры", wikis=wikis)
 
 
 @wiki.route('/wiki/<int:id>')
 def get_one_wiki(id):
     cur_wiki = databaserequest(f'''SELECT id, title, author, content FROM wiki WHERE id='{id}' ''')
-    return render_template('wiki/cur_wiki.html', wiki=cur_wiki)
+    return render_template('wiki/wiki_page.html', session=session,
+                           title=f"{cur_wiki['title']} - База данных Ишимуры", wiki=cur_wiki)
 
 
 @wiki.route('/wiki_add', methods=['GET', 'POST'])
 def add_wiki():
     if request.method == 'POST':
         try:
-            title, content = request.form.get('title'), request.form.get('ckeditor')
+            title, content, category = (request.form.get('title'),
+                                        request.form.get('ckeditor'),
+                                        request.form.get('category'))
             date_time = datetime.date.today()
             author = session['nickname']
 
-            databaserequest(f'''INSERT INTO wiki(title, content, author, datetime) 
-            VALUES('{title}', '{content}', '{author}', '{date_time}')''')
+            databaserequest(f'''INSERT INTO wiki(title, content, author, datetime, category) 
+            VALUES('{title}', '{content}', '{author}', '{date_time}', '{category}')''')
             return redirect('/wiki')
         except KeyError:
             abort(413)
-    return render_template('wiki/add.html')
+    return render_template('wiki/add.html', session=session, title="Добавление вики-страницы")
 
 
-@wiki.route('/wiki_redact/<int:id>', methods=['GET', 'POST'])
-def redact_wiki(id):
+@wiki.route('/wiki_edit/<int:id>', methods=['GET', 'POST'])
+def edit_wiki(id):
     cur_wiki = databaserequest(f'''SELECT title, author, content FROM wiki WHERE id='{id}' ''')
     if request.method == 'POST':
         try:
-            title, content = request.form.get('title'), request.form.get('ckeditor')
+            title, content, category = (request.form.get('title'),
+                                        request.form.get('ckeditor'),
+                                        request.form.get('category'))
             date_time = datetime.date.today()
             author = session['nickname']
 
             databaserequest(f'''UPDATE wiki SET title='{title}', content='{content}', 
-            datetime='{date_time}', author='{author}' WHERE id='{id}' ''')
+            datetime='{date_time}', author='{author}', category='{category}' WHERE id='{id}' ''')
             return redirect('/wiki')
         except KeyError:
             abort(413)
-    return render_template('wiki/redact.html', cur_wiki=cur_wiki, id=id)
+    return render_template('wiki/edit.html', session=session, title="Редактирование вики-страницы",
+                           cur_wiki=cur_wiki, id=id)
 
 
 @wiki.route('/wiki_delete/<int:id>', methods=['GET', 'POST'])
